2619d0bedfc8b54d15dc0a555ef177ee2f0ec374
Richardson Moraes <38991199+RichardsonMM@users.noreply.github.com>
2018-08-27 10:06:19 -0300

    Create fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+using namespace std;
+
+int fibonacci(int n){
+	if(n == 1)
+		return 0;
+	else if(n == 2 || n == 2)
+		return 1;
+	else
+		return fibonacci(n-1)+fibonacci(n-2);
+}
+
+int main(){
+
+	int n;
+
+	scanf("%d", &n);
+
+	printf("%d\n", fibonacci(n));
+
+	return 0;
+}
