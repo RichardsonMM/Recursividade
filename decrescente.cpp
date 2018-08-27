6a03013a3c4486a26569afd435f7f98ad5d53a71
Richardson Moraes <38991199+RichardsonMM@users.noreply.github.com>
2018-08-27 08:59:14 -0300

    Create decrescente.cpp

---
+++ decrescente.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+using namespace std;
+
+
+void decrescente(int n){
+	if(n >= 1){
+		printf("%d\n", n);
+		n--;
+		return decrescente(n);
+	}
+}
+
+int main(){
+
+	int n;
+
+	scanf("%d", &n);
+
+	decrescente(n);
+
+	return 0;
+}
