41ef2dbb3ffbdb0d8880926fde55589136f2e72a
Richardson Moraes <38991199+RichardsonMM@users.noreply.github.com>
2018-08-28 18:38:24 -0300

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int fibonacci(int n){
 	if(n == 1)
 		return 0;
-	else if(n == 2 || n == 2)
+	else if(n == 1 || n == 2)
 		return 1;
 	else
 		return fibonacci(n-1)+fibonacci(n-2);
